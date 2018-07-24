6ae7de418934eb1a528888d3d59d2ebf3e896afc
Maxime <potomane555@gmail.com>
2018-07-24 22:31:56 +0200

    Fix code proportion

--- atom.mk
+++ atom.mk
@@ -1,15 +1,9 @@
 ###############################################################################
-
 LOCAL_PATH := $(call my-dir)
-
 ###############################################################################
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := tup
-
 LOCAL_SRC_FILES := test_unique_ptr.cpp
-
 LOCAL_CXXFLAGS := -std=c++11
-
 include $(BUILD_EXECUTABLE)
